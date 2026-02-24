1f3875005ef023a0342cdee1fd885dd58c227c8b
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-02-24 15:50:15 -0500

    contact

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -65,7 +65,6 @@ class HomeController extends Controller
         $request->validate([
             'name'    => 'required|string|max:255',
             'email'   => 'required|email',
-            'subject' => 'required|string|max:255',
             'message' => 'required|string',
         ]);
        
